e219387d17564e418128fa7a5cfc76f81f3170c0
maiconda <maicondeoliveiradasilva24@gmail.com>
2023-11-19 13:02:47 -0300

    V2

--- main.c
+++ main.c
@@ -1,44 +1,96 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 #define TAMANHO_TABELA 53
 #define TAMANHO_PALAVRA 50
 
 typedef struct No {
-    char* palavra;
-    struct No* proximo;
-    struct No* anterior;
+    char *palavra;
+    struct No *proximo;
+    struct No *anterior;
 } No;
 
 typedef struct TabelaHash {
-    No* slots[TAMANHO_TABELA];
+    No *slots[TAMANHO_TABELA];
 } TabelaHash;
 
-int hash(char chave[]) {
+int hash(char palavra[]);
+void inicializarTabelaHash(TabelaHash *tabela);
+void inserirFunction(TabelaHash *tabela, char palavra[]);
+void insercaoInicial(TabelaHash *tabela);
+void insercaoPalavras(TabelaHash *tabela);
+void excluirDaTabela(TabelaHash *tabela);
+void imprimirTabelaHash(TabelaHash *tabela);
+void buscarNaTabela(TabelaHash *tabela);
+void exibirMenu();
+
+int main() {
+    TabelaHash minhaTabela;
+    inicializarTabelaHash(&minhaTabela);
+    insercaoInicial(&minhaTabela);
+
+    int opcao;
+    do {
+        exibirMenu();
+        printf("Escolha uma opção: ");
+        scanf("%d", &opcao);
+
+        switch (opcao) {
+            case 1:
+                insercaoPalavras(&minhaTabela);
+                break;
+
+            case 2:
+                excluirDaTabela(&minhaTabela);
+                break;
+
+            case 3:
+                imprimirTabelaHash(&minhaTabela);
+                break;
+
+            case 4:
+                buscarNaTabela(&minhaTabela);
+                break;
+
+            case 5:
+                printf("Saindo do programa.\n");
+                break;
+
+            default:
+                printf("Opção inválida. Tente novamente.\n");
+        }
+    } while (opcao != 5);
+    return 0;
+}
+
+int hash(char palavra[]) {
     int soma = 0;
-    for (int i = 0; chave[i] != '\0'; i++) {
-        soma += chave[i];
+    for (int i = 0; palavra[i] != '\0'; i++) {
+        soma += palavra[i];
     }
     return soma % TAMANHO_TABELA;
 }
 
-void inicializarTabelaHash(TabelaHash* tabela) {
+void inicializarTabelaHash(TabelaHash *tabela) {
     for (int i = 0; i < TAMANHO_TABELA; i++) {
         tabela->slots[i] = NULL;
     }
 }
 
-void inserirFunction(TabelaHash* tabela, char* palavra) {
+void inserirFunction(TabelaHash *tabela, char palavra[]) {
     int indice = hash(palavra);
 
-    No* novoNo = (No*)malloc(sizeof(No));
+    No *novoNo = (No *) malloc(sizeof(No));
+
     if (novoNo == NULL) {
         fprintf(stderr, "Erro na alocação de memória.\n");
         exit(EXIT_FAILURE);
     }
 
     novoNo->palavra = strdup(palavra);
+
     if (novoNo->palavra == NULL) {
         fprintf(stderr, "Erro na alocação de memória.\n");
         free(novoNo);
@@ -55,8 +107,9 @@ void inserirFunction(TabelaHash* tabela, char* palavra) {
     tabela->slots[indice] = novoNo;
 }
 
-void insercaoInicial(TabelaHash* tabela) {
-    FILE* fptr = fopen("../nomes.txt", "r");
+void insercaoInicial(TabelaHash *tabela) {
+
+    FILE *fptr = fopen("../nomes.txt", "r");
     if (fptr == NULL) {
         fprintf(stderr, "Erro ao abrir o arquivo.\n");
         exit(EXIT_FAILURE);
@@ -65,7 +118,7 @@ void insercaoInicial(TabelaHash* tabela) {
     char palavra[TAMANHO_PALAVRA];
 
     while (fgets(palavra, sizeof(palavra), fptr) != NULL) {
-        // Remover a quebra de linha, se existir
+
         size_t len = strlen(palavra);
         if (len > 0 && palavra[len - 1] == '\n') {
             palavra[len - 1] = '\0';
@@ -77,10 +130,10 @@ void insercaoInicial(TabelaHash* tabela) {
     fclose(fptr);
 }
 
-void insercaoPalavras(TabelaHash* tabela) {
+void insercaoPalavras(TabelaHash *tabela) {
     int tamanho;
 
-    printf("Digite quantas palavras deseja adicionar: ");
+    printf("\nDigite quantas palavras deseja adicionar: ");
     scanf("%d", &tamanho);
 
     if (tamanho <= 0) {
@@ -94,20 +147,30 @@ void insercaoPalavras(TabelaHash* tabela) {
         scanf("%s", palavras[i]);
     }
 
-    printf("\nConteúdo do vetor de strings:\n");
+
+    for (int i = 0; i < tamanho; i++) {
+        for (int j = 0; palavras[i][j] != '\0'; j++) {
+            palavras[i][j] = toupper(palavras[i][j]);
+        }
+    }
+
     for (int i = 0; i < tamanho; i++) {
         inserirFunction(tabela, palavras[i]);
     }
 
-    return;  // Sair do programa com sucesso
+    printf("Palavras adicionadas!\n");
 }
 
-void excluirDaTabela(TabelaHash* tabela) {
+void excluirDaTabela(TabelaHash *tabela) {
 
     char palavra[TAMANHO_PALAVRA];
 
-    printf("Digite a palavra a ser excluida:");
+    printf("\nDigite a palavra a ser excluida:");
     scanf("%s", palavra);
+
+    for (int i = 0; palavra[i] != '\0'; i++) {
+        palavra[i] = toupper(palavra[i]);
+    }
 
     int indice = hash(palavra);
 
@@ -116,7 +179,7 @@ void excluirDaTabela(TabelaHash* tabela) {
         exit(EXIT_FAILURE);
     }
 
-    No* atual = tabela->slots[indice];
+    No *atual = tabela->slots[indice];
 
     while (atual != NULL) {
         if (strcmp(atual->palavra, palavra) == 0) {
@@ -143,10 +206,11 @@ void excluirDaTabela(TabelaHash* tabela) {
     printf("Palavra \"%s\" não encontrada na tabela.\n", palavra);
 }
 
-void imprimirTabelaHash(TabelaHash* tabela) {
+void imprimirTabelaHash(TabelaHash *tabela) {
+    printf("\n");
     for (int i = 0; i < TAMANHO_TABELA; i++) {
         int contador = 0;
-        No* atual = tabela->slots[i];
+        No *atual = tabela->slots[i];
         while (atual != NULL) {
             contador++;
             atual = atual->proximo;
@@ -155,48 +219,44 @@ void imprimirTabelaHash(TabelaHash* tabela) {
     }
 }
 
+void buscarNaTabela(TabelaHash *tabela) {
+
+    char palavra[TAMANHO_PALAVRA];
+
+    printf("\nDigite a palavra a ser buscada: ");
+    scanf("%s", palavra);
+
+    for (int i = 0; palavra[i] != '\0'; i++) {
+        palavra[i] = toupper(palavra[i]);
+    }
+
+    int indice = hash(palavra);
+    int posicaoSlot = 0;
+
+    if (indice < 0 || indice >= TAMANHO_TABELA) {
+        fprintf(stderr, "Índice fora dos limites da tabela.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    No *atual = tabela->slots[indice];
+
+    while (atual != NULL) {
+        if (strcmp(atual->palavra, palavra) == 0) {
+            printf("A palavra '%s' foi encontrada no slot '%d' na posição '%d'\n", palavra, indice, posicaoSlot);
+            return;
+        }
+
+        atual = atual->proximo;
+        posicaoSlot++;
+    }
+    printf("Palavra não econtrada\n");
+}
+
 void exibirMenu() {
     printf("\nMenu:\n");
     printf("1. Inserir palavras\n");
     printf("2. Excluir palavra\n");
     printf("3. Imprimir tabela\n");
-    printf("4. Sair\n");
-}
-
-int main() {
-    TabelaHash minhaTabela;
-    inicializarTabelaHash(&minhaTabela);
-
-//    insercaoInicial(&minhaTabela);
-
-    int opcao;
-
-    do {
-        exibirMenu();
-        printf("Escolha uma opção: ");
-        scanf("%d", &opcao);
-
-        switch (opcao) {
-            case 1:
-                insercaoPalavras(&minhaTabela);
-                break;
-
-            case 2:
-                excluirDaTabela(&minhaTabela);
-                break;
-
-            case 3:
-                imprimirTabelaHash(&minhaTabela);
-                break;
-
-            case 4:
-                printf("Saindo do programa.\n");
-                break;
-
-            default:
-                printf("Opção inválida. Tente novamente.\n");
-        }
-    } while (opcao != 4);
-
-    return 0;
+    printf("4. Buscar na tabela\n");
+    printf("5. Sair\n");
 }
